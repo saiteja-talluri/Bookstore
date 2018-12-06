60495ad8800684a6865689b51cc5bd89c2e16286
saiteja-talluri <saitejatalluri836@gmail.com>
2018-12-06 18:01:14 +0530

    Updated Repo

--- blog/urls.py
+++ blog/urls.py
@@ -9,9 +9,9 @@ from .forms import SignUpForm,InterestForm,DocumentForm,RatingForm,CommunityForm
 form1=SignUpForm()
 urlpatterns = [
     url(r'^$', entry_views.home, name='home'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html', 'extra_context': {'form1':form1 ,},},
+    url(r'^login/$', auth_views.LoginView, {'template_name': 'login.html', 'extra_context': {'form1':form1 ,},},
          name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^logout/$', auth_views.LogoutView, {'next_page': 'login'}, name='logout'),
     url(r'^signup/$', entry_views.signup, name='signup'),
     #url(r'^chat/dialogs/(?P<pk>\w+)/$', entry_views.chat, name='chat'),
     
@@ -46,11 +46,11 @@ urlpatterns = [
  	url(r'^login/acceptread/(?P<pk>\d+)/(?P<pk1>\d+)/$',entry_views.acceptread,name='acceptread'),
  	url(r'^login/upfileinc/(?P<pk>\d+)/$',	entry_views.upfileinc,name='upfileinc'),
     url(r'^login/leave/(?P<pk>\w+)/$',entry_views.leave,name='leave'),
-    url(r'^password_reset/$', auth_views.password_reset,{'template_name': 'password_reset_form.html'},name='password_reset'), 
-    url(r'^password_reset/done/$', auth_views.password_reset_done,{'template_name': 'password_reset_done.html'},name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm,
+    url(r'^password_reset/$', auth_views.PasswordResetView,{'template_name': 'password_reset_form.html'},name='password_reset'), 
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView,{'template_name': 'password_reset_done.html'},name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.PasswordResetConfirmView,
     	{'template_name': 'password_reset_confirm.html'},name='password_reset_confirm'),
-	url(r'^reset/done/$', auth_views.password_reset_complete,{'template_name': 'password_reset_complete.html'},name='password_reset_complete'),    
+	url(r'^reset/done/$', auth_views.PasswordResetCompleteView,{'template_name': 'password_reset_complete.html'},name='password_reset_complete'),    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
